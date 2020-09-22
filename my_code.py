039ffde2af94bdd37cfe9c3133246a7aa7e25dec
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-09-21 18:30:36 -0700

    update starter, test code

---
+++ my_code.py
@@ -0,0 +1,18 @@
+# Collaborators (including web sites where you got help: (enter none if you didn't need help)
+#
+
+def avg(user_list):
+    total = sum(user_list)
+    num_values = len(user_list)
+    average = total/num_values
+    return average
+
+
+if __name__ == '__main__':
+    # test your fucntion with this print statement before writing the input loop
+    print(avg([x, y, z]))    # Put the values you want to test in for x,y and z
+
+    # Now, comment out the print statement and work on the code below
+    # Remember to indent in this section
+    user_list = [] # start with an empty list
+    # Write a loop to allow the user to input the values to be averaged
--- my_test.py
+++ my_test.py
@@ -1,7 +1,6 @@
-from my_code import inc
+from my_code import avg
 
-
-def test_inc():
-    assert 5 == inc(4)
-    assert 1 == inc(0)
-    assert 0 == inc(-1)
+def test_avg():
+    assert 3 == avg([1,5,2,3,4])
+    assert 25.77777777777778 == avg([10, 25, 12, 3, 1, 0, -22, 176, 27])
+    assert 5.033333333333334 == avg([1.6, 16.5, -2.5, 7, 3.6, 4])
